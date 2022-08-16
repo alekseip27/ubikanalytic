fca51ba40eacabaa2d6b965d84dd5e0a5c6e3f69
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-08-16 15:18:58 +0900

    Update and rename poq.js to poqs.js

--- poqs.js
+++ poqs.js
@@ -146,7 +146,7 @@ Webflow.push(function() {
 const savepricebutton = card.getElementsByClassName('save-price-button')[0]   
 savepricebutton.addEventListener('click', function() {
 let activeticket = ($(this).closest(".event-box-pricing").attr('id'));
-let price = Number(($(this).closest(".main-field-price").val()))
+let price = ($(this).closest(".main-field-price").val())
 var http = new XMLHttpRequest();
 var url = "https://x828-xess-evjx.n7.xano.io/api:Owvj42bm/update-price?ticket-id=" + activeticket + "&price=" + price;
 let pa = datas['pyeo']
