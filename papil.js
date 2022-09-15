1d67f29d5e2d77f21b55e83117b35491c0a1f0e4
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2022-09-16 03:24:29 +0900

    Update and rename papi.js to papil.js

--- papil.js
+++ papil.js
@@ -157,10 +157,10 @@ Webflow.push(function() {
     eventprice.value = events.listPrice
 
     const eventpriceticket = card.getElementsByClassName('main-text-priceticket')[0]
-    let dticket = (events.cost/events.quantity)
+    let dticket = String((events.cost/events.quantity))
 
 
-    let pti = String(dticket).split(".");
+    let pti = dticket).split(".");
 
     let ptix = pti[0] + '.' + pti[1].slice(0,2)
 
