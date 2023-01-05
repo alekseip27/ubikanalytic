6565890aa49c363eb0da06194145e0b078346e94
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-01-05 18:39:32 +0200

    Update and rename mainz.js to mainzz.js

--- mainzz.js
+++ mainzz.js
@@ -87,32 +87,31 @@ Webflow.push(function() {
 
                 
 
-    const getchartsd = async function(){
-        let dates_sd = []
-        let amounts_sd = []
-        
-        let eventid = events.stubhubEventUrl.slice(-10,-1)
-        let eventurl = events.stubhubEventUrl
-        let getevent = ('https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5/seatdata_0?eventid=') +  eventid + "&Event_Url=" + eventurl;
-        
-        let response = await fetch(getevent);
-        let commits = await response.json()
-        
+ const getchartsd = async function(){
+    let dates_sd = []
+    let amounts_sd = []
+    
+    let eventid = events.stubhubEventUrl.slice(-10,-1)
+    let eventurl = events.stubhubEventUrl
+    let getevent = ('https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5/seatdata_0?eventid=') +  eventid + "&Event_Url=" + eventurl;
+    
+    let response = await fetch(getevent);
+    let commits = await response.json()
+    
 for (let commit of commits) {
-    amounts_sd.push(commit.quantity)
-    dates_sd.push(moment.unix(commit.timestamp).format("MM/DD/YYYY hh:mm"))
+amounts_sd.push(commit.quantity)
+dates_sd.push(moment.unix(commit.timestamp).format("MM/DD/YYYY hh:mm"))
 }
 
 // Sort the dates in ascending order
 dates_sd.sort((a, b) => {
-    return new Date(a) - new Date(b);
+return new Date(a) - new Date(b);
 });
 
 chart.data.datasets[0].data = amounts_sd
 chart.config.data.labels = dates_sd
 chart.update();
 
-        }
     }
     
 const getchartvs = async function(){
