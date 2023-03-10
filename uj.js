50e27c01de2ee5707f61e52ba75a9ae3f490e15d
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-03-10 19:42:05 +0900

    Update and rename mmm.js to uj.js

--- uj.js
+++ uj.js
@@ -176,8 +176,8 @@ const primaryurl = async function(){
 let getevent = 'https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5:v1/getevent_primaryurl?search-key='+events.venue.id+events.date.slice(0,10)
 let response = await fetch(getevent);
 let commits = await response.json()
-console.log(commits)
-console.log(card.getAttribute('id'))
+console.log(commits.Other_Master_Event_Url)
+document.querySelector('#urlmain').setAttribute('url',commits.Other_Master_Event_Url);
 }
 
 
