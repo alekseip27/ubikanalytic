38979a79afa5789cdd183876ffb8671ac031e304
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-03-11 16:36:36 +0900

    Update and rename hyat.js to hyatt.js

--- hyatt.js
+++ hyatt.js
@@ -174,6 +174,7 @@ const primaryurl = async function(){
 let getevent = 'https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5:v1/getevent_primaryurl?search-key='+events.venue.id+events.date.slice(0,10)+'&search-key2='+events.venue.name+'&search-key3='+events.date.slice(0,10)
 let response = await fetch(getevent);
 let commits = await response.json()
+console.log(commits)
 if(commits.length>0){
 document.querySelector('#urlmain').setAttribute('url',commits[0].Other_Master_Event_Url)
 document.querySelector('#urlmain').style.display = 'flex'
