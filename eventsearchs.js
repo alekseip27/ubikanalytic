c87770a313cfbee730b0ca1da6da1f844216246b
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-03-22 16:24:36 +0900

    Update and rename eventsearch.js to eventsearchs.js

--- eventsearchs.js
+++ eventsearchs.js
@@ -191,6 +191,8 @@ primaryurl()
 
 
 if(events.Event_Other_Master_Source_Formula == 'TM' && !evid.startsWith('Z') && evid.length == 16) {
+primrem.textContent = '0'
+dpd.textContent = '0'
 primaryurl()
 rescrapebutton.style.display = 'flex'
 scrapebutton.style.display = 'flex'
