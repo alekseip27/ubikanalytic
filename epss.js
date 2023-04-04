67f4c5c1672ec461a5cfc3488d6b87f3c5a89366
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-04-04 21:18:37 +0300

    Update and rename eps.js to epss.js

--- epss.js
+++ epss.js
@@ -98,8 +98,7 @@ this.remove()
   request.onload = function() {
   $('#search-button').css({pointerEvents: "auto"})
   let data = JSON.parse(this.response)
-  if((request.status === 429) || (request.status === 500)){
-  else if(request.status === 401){
+  if(request.status === 401){
   document.querySelector(".locked-content").style.display = 'flex'
   document.querySelector(".pageloading").style.display = 'none'
   } else if (request.status >= 200 && request.status < 400) {
