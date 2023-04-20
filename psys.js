dff0089016791357d3f91fd492c2252beaf05dbc
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-04-21 00:54:26 +0700

    Update and rename psystem.js to psys.js

--- psys.js
+++ psys.js
@@ -160,20 +160,27 @@ Webflow.push(function() {
   document.querySelector('.chart-loading').style.display = 'none'
   }
   
-  function getvenuedata(){
+ function getvenuedata(){
+  document.getElementById('venueresale').textContent = ''
+  document.getElementById('venuecap').textContent = ''
+  document.getElementById('fwicon').textContent = ''
+  document.getElementById('lowcaptext').textContent = ''
+  
 
-let getevent = 'https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5/getevent_byvdid?search-key='+ events.venue.id + events.date.slice(0,10)
+let getevent = 'https://x828-xess-evjx.n7.xano.io/api:Bwn2D4w5/getevent_byvdid?search-key='+"99182023-04-21";
 
 fetch(getevent)
     .then(response => response.json())
     .then(commits => {
 if(!!commits){
+  document.getElementById('fwicon').textContent = ''
+  document.getElementById('lowcaptext').textContent = '/'
   document.querySelector('#venuebox').style.display = 'flex'
 let venuecap = commits[0].Venue_Master_Venue_Capacity
 document.querySelector('#venuename').textContent = commits[0].Venue_Master_Venue
 
 
-let geteventparttwo = 'https://x828-xess-evjx.n7.xano.io/api:Owvj42bm/vividseats_data?id=' +card.getAttribute('id')
+let geteventparttwo = 'https://x828-xess-evjx.n7.xano.io/api:Owvj42bm/vividseats_data?id=' + '4299857';
 
 fetch(geteventparttwo)
 .then(response => response.json())
