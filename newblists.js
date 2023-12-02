83d4be9a1089f3cacef3ea7637b2c9deabed08c0
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2023-12-02 16:57:33 +0200

    Update and rename newblist.js to newblists.js

--- newblists.js
+++ newblists.js
@@ -1,4 +1,4 @@
-let xanoUrl = new URL('https://ubik.wiki/api/buying-queue/?completed__iexact=false');
+let xanoUrl = new URL('https://ubik.wiki/api/buying-queue/?completed__iexact=false&limit=1000');
 function getEvents() {
 
 let request = new XMLHttpRequest();
