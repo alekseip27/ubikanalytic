3c543414bcbd12797ac97d2ecc1a3d6e68f614fd
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2024-02-08 19:58:49 +0200

    Update and rename main-eventq.js to main-eventl.js

--- ubik/main-eventl.js
+++ ubik/main-eventl.js
@@ -316,10 +316,9 @@ document.querySelector('#vserror').style.display = 'flex';
           .then(data => {
               let event = data.results[0];
               let counts = event.counts;
-            let source; 
-            if(event.scraper_name){
-              let source = event.scraper_name.toLowerCase()
-            }
+
+              let source = event.scraper_name
+            
               chart.data.datasets[0].label = source.toUpperCase() + ' Primary'
               evids = event.site_event_id
               if (counts && counts.length > 0 && !source.includes('tm')) {
