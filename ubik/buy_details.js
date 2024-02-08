162b7ad6bbb1c9ed1f91e8ef675493feac91b1ed
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2024-02-08 19:55:25 +0200

    Update and rename buy-details.js to buy_details.js

--- ubik/buy_details.js
+++ ubik/buy_details.js
@@ -606,7 +606,7 @@ const checkStepsInterval = setInterval(() => {
 
 
 function getaccounts() {
-  let url = new URL('https://ubik.wiki/api/buyer-emails/?one1ticket_add__iexact=true&tm_added__iexact=true&one1ticket_verify__iexact=true&second_forward_verify__iexact=true&retired__iexact=false&email_suspended__iexact=false');
+  let url = new URL('https://ubik.wiki/api/buyer-emails/?one1ticket_add__iexact=true&tm_added__iexact=true&one1ticket_verify__iexact=true&second_forward_verify__iexact=true&retired__iexact=false&email_suspended__iexact=false&limit=1000');
   let request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.setRequestHeader("Content-type", "application/json; charset=utf-8");
