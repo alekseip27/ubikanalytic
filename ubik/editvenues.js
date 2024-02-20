88dd7f6bf944f7dce939ad53bfd30a4649b891b6
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2024-02-20 17:16:01 +0200

    Update and rename edit-venuez.js to editvenues.js

--- ubik/editvenues.js
+++ ubik/editvenues.js
@@ -213,7 +213,7 @@ function checkresults() {
     deletebutton.addEventListener('click', function() {
     document.querySelector('.edit-wrapper').style.display = 'none';
 
-    const url = `https://ubik.wiki/api/delete/venues/${evids}/`;
+    const url = `https://ubik.wiki/api/delete/venues/
 
     fetch(url, {
         method: 'DELETE',
