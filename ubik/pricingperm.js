d9835753f6f7f88a124a0f4710d3a236f5c04513
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2024-06-04 16:17:55 +0900

    Update and rename pricepermission.js to pricingperm.js

--- ubik/pricingperm.js
+++ ubik/pricingperm.js
@@ -70,6 +70,7 @@ if (email === 'aleksei@ubikanalytic.com' || email === 'tim@ubikanalytic.com') {
     document.querySelector('#lowerbox').style.display = 'none';
     document.querySelector('#searchblock').style.display = 'none';
     document.querySelector('.totalcounts').style.display = 'none';
+    document.querySelector('.chart-tab').style.display = 'none'
     document.querySelector('#lowerable').checked = true;
 }
 
