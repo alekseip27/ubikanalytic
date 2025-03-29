419ceddd3821a5082aab71b4a181647a0129fc16
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2025-03-29 16:46:47 +0900

    Update and rename S5I1.js to S5I2.js

--- ubik/S5I2.js
+++ ubik/S5I2.js
@@ -36,59 +36,71 @@ card.style.display = 'flex'
 card.setAttribute('id', events.id);
 card.setAttribute('checked','false')
 
-const instructions = events.instructions;
-const contains = events.contains;
-const browser = events.browser;
-const source = events.source;
 const id = events.id;
+const source = events.source;
 const eventprefix = events.event_prefix;
 const venueprefix = events.venue_prefix;
+const browser = events.browser;
+const instructions = events.instructions;
 const backupone = events.backup_one;
 const backuptwo = events.backup_two;
+const contains = events.contains;
+const funct = events.function;
+
 
 const sourcecard = card.getElementsByClassName('main-text-sourcei')[0]
 sourcecard.textContent = source
-
-const containscard = card.getElementsByClassName('main-text-contains')[0]
-containscard.textContent = contains
-
-const browsercard = card.getElementsByClassName('main-text-browser')[0]
-browsercard.textContent = browser
-
-const instructionscard = card.getElementsByClassName('main-text-instruction')[0]
-instructionscard.textContent = instructions
-
-const backupcardone = card.getElementsByClassName('main-text-backup1')[0]
-backupcardone.textContent = backupone
-
-const backupcardtwo = card.getElementsByClassName('main-text-backup2')[0]
-backupcardtwo.textContent = backuptwo
-
-
-
     
 const eventprefixcard = card.getElementsByClassName('main-text-eprefix')[0]
 eventprefixcard.textContent = eventprefix
 
 const venueprefixcard = card.getElementsByClassName('main-text-vprefix')[0]
 venueprefixcard.textContent = venueprefix
+    
+const browsercard = card.getElementsByClassName('main-text-browser')[0]
+browsercard.textContent = browser
+
+const instructionscard = card.getElementsByClassName('main-text-instruction')[0]
 
 if(instructions.length > 33) {
 instructionscard.textContent = instructions.slice(0, 30) + '...'
 } else {
 instructionscard.textContent = instructions
 }
+    
+const backupcardone = card.getElementsByClassName('main-text-backup1')[0]
+backupcardone.textContent = backupone
+
+const backupcardtwo = card.getElementsByClassName('main-text-backup2')[0]
+backupcardtwo.textContent = backuptwo
+    
+const containscard = card.getElementsByClassName('main-text-contains')[0]
+if(contains.length > 33) {
+containscard.textContent = contains.slice(0, 30) + '...'
+} else {
+containscard.textContent = contains
+}
+
+const functioncard = card.getElementsByClassName('main-text-function')[0]
+if(contains.length > 33) {
+functioncard.textContent = funct.slice(0, 30) + '...'
+} else {
+functioncard.textContent = funct
+}
+
+    
 
 card.addEventListener('click', function(){
     document.querySelector('#edit-id').value = ''
     document.querySelector('#edit-source').value = ''
-    document.querySelector('#edit-browser').value = ''
     document.querySelector('#edit-event-prefix').value = ''
     document.querySelector('#edit-venue-prefix').value = ''
+    document.querySelector('#edit-browser').value = ''
     document.querySelector('#edit-instructions').value = ''
-    document.querySelector('#edit-contains').value = ''
     document.querySelector('#edit-backup1').value = ''
     document.querySelector('#edit-backup2').value = ''
+    document.querySelector('#edit-contains').value = ''
+    document.querySelector('#edit-function').value = ''
 
     if (event.target.closest('.main-text-delete')) {
     return;
@@ -99,13 +111,14 @@ card.addEventListener('click', function(){
 
     document.querySelector('#edit-id').value = id
     document.querySelector('#edit-source').value = source
-    document.querySelector('#edit-browser').value = browser
-    document.querySelector('#edit-instructions').value = instructions
-    document.querySelector('#edit-contains').value = contains
     document.querySelector('#edit-event-prefix').value = eventprefix
     document.querySelector('#edit-venue-prefix').value = venueprefix
+    document.querySelector('#edit-browser').value = browser
+    document.querySelector('#edit-instructions').value = instructions
     document.querySelector('#edit-backup1').value = backupone
     document.querySelector('#edit-backup2').value = backuptwo
+    document.querySelector('#edit-contains').value = contains
+    document.querySelector('#edit-function').value = funct
     })
 
 
@@ -202,25 +215,27 @@ document.querySelector('#editbtn').addEventListener('click', function() {
     document.querySelector('#editbtn').style.pointerEvents = "none";
 let itemid = document.querySelector('#edit-id').value
 let source = document.querySelector('#edit-source').value
-let browser = document.querySelector('#edit-browser').value
-let instructions = document.querySelector('#edit-instructions').value
-let contains = document.querySelector('#edit-contains').value
-let backupone = document.querySelector('#edit-backup1').value
-let backuptwo = document.querySelector('#edit-backup2').value
 let eventprefix = document.querySelector('#edit-event-prefix').value
 let venueprefix = document.querySelector('#edit-venue-prefix').value
+let browser = document.querySelector('#edit-browser').value
+let instructions = document.querySelector('#edit-instructions').value
+let backupone = document.querySelector('#edit-backup1').value
+let backuptwo = document.querySelector('#edit-backup2').value
+let contains = document.querySelector('#edit-contains').value
+let funct = document.querySelector('#edit-function').value
 const endpointUrl = 'https://ubik.wiki/api/update/source-instructions/'
 
 const newRowData = {
 id: itemid,
 source: source,
+event_prefix: eventprefix,
+venue_prefix: venueprefix,
 browser: browser,
 instructions: instructions,
-contains: contains,
 backup_one: backupone,
 backup_two: backuptwo,
-event_prefix: eventprefix,
-venue_prefix: venueprefix
+contains: contains,
+function: funct
 };
 
 
@@ -267,12 +282,14 @@ function createInstruction() {
     const endpointUrl = 'https://ubik.wiki/api/create/source-instructions/'
 
     let source = document.querySelector('#add-source').value
-    let browser = document.querySelector('#add-browser').value
-    let instructions = document.querySelector('#add-instructions').value
     let vprefix = document.querySelector('#add-venue-prefix').value
     let eprefix = document.querySelector('#add-event-prefix').value
+    let browser = document.querySelector('#add-browser').value
+    let instructions = document.querySelector('#add-instructions').value
     let backupone = document.querySelector('#add-backup1').value
     let backuptwo = document.querySelector('#add-backup2').value
+    let contains = document.querySelector('#add-contains').value
+    let funct = document.querySelector('#add-function').value
 
     const newRowData = {
         source: source,
@@ -282,6 +299,8 @@ function createInstruction() {
         venue_prefix: vprefix,
         backup_one: backupone,
         backup_two: backuptwo,
+        contains: contains,
+        function: funct
     };
 
     fetch(endpointUrl, {
@@ -317,16 +336,20 @@ document.getElementById('editbtn2').addEventListener('click', function() {
 
 
 document.getElementById('addnew').addEventListener('click', function() {
+
+    
 document.querySelector('#editbtn2').style.pointerEvents = "none";
 document.querySelector('#editwrapper2').style.display = 'flex';
 document.querySelector('#errortext2').textContent = ''
 document.querySelector('#add-source').value = ''
+document.querySelector('#add-venue-prefix').value = ''
+document.querySelector('#add-event-prefix').value = '' 
 document.querySelector('#add-browser').value = ''
+document.querySelector('#add-instructions').value = ''
 document.querySelector('#add-backup1').value = ''
 document.querySelector('#add-backup2').value = ''
-document.querySelector('#add-instructions').value = ''
-document.querySelector('#add-venue-prefix').value = ''
-document.querySelector('#add-event-prefix').value = ''
+document.querySelector('#add-contains').value = ''
+document.querySelector('#add-function').value = ''
 });
 
 
