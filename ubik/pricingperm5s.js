2870da3226bc29189cce23f75deaab8033ce2f8c
alekseip27 <94841268+alekseip27@users.noreply.github.com>
2026-04-15 14:00:04 +0400

    Update and rename pricingperm4s.js to pricingperm5s.js

--- ubik/pricingperm5s.js
+++ ubik/pricingperm5s.js
@@ -98,7 +98,7 @@ if (email === 'aleksei@ubikanalytic.com' || email === 'tim@ubikanalytic.com') {
 }
 
 
-    if(email === 'aleksei@ubikanalytic.com' || email === 'tim@ubikanalytic.com' || email === 'aa@ubikanalytic.com'  || email === 'jm@ubikanalytic.com'){
+    if(email === 'aleksei@ubikanalytic.com' || email === 'tim@ubikanalytic.com'){
     document.querySelector('.totalcounts').style.display = 'flex'
     document.querySelector('#lowerbox').style.display = 'flex'
     } else {
